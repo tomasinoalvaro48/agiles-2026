1d7b5ddd84c2ec2f5059c3df284a3fcb99875d8a
Alvaro Tomasino <alvaroyaugusto@gmail.com>
2026-05-11 20:33:26 -0300

    Inicial

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/*.test.ts', '**/*.spec.ts', '**/test.ts'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: false,
+        tsconfig: {
+          module: 'commonjs',
+        },
+      },
+    ],
+  },
+}
