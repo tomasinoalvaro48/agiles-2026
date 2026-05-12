14b268accf4f60748333d06025578a248eb072f8
Tomas <tomas.aguilera.27@gmail.com>
2026-05-11 21:18:58 -0300

    arreglo test 4

--- src/SumaDeStrings.ts
+++ src/SumaDeStrings.ts
@@ -1,9 +1,13 @@
 class SumaDeStrings {
   Sumar(numeros: string) {
-    const array = numeros.split(',');
     let suma = 0;
-    for (let i = 0; i < array.length; i++) {
-      suma += parseInt(array[i]);
+
+    if (numeros !== '') {
+      const array = numeros.split(',');
+
+      for (let i = 0; i < array.length; i++) {
+        suma += parseInt(array[i]);
+      }
     }
     return suma;
   }
