1c085043dfcef23c5de92ef84ce382c525f1d51d
Tomas <tomas.aguilera.27@gmail.com>
2026-05-11 21:18:41 -0300

    test 4

--- src/test.ts
+++ src/test.ts
@@ -1,17 +1,22 @@
-import { expect, test } from '@jest/globals'
-import SumaDeStrings from './SumaDeStrings'
+import { expect, test } from '@jest/globals';
+import SumaDeStrings from './SumaDeStrings';
 
 test('Suma de strings: "" debe ser 0', () => {
-    const suma = new SumaDeStrings()
-    expect(suma.Sumar("")).toBe(0)
-})
+  const suma = new SumaDeStrings();
+  expect(suma.Sumar('')).toBe(0);
+});
 
 test("Suma de strings: '1,2' debe ser 3", () => {
-    const suma = new SumaDeStrings()
-    expect(suma.Sumar("1,2")).toBe(3)
-})
+  const suma = new SumaDeStrings();
+  expect(suma.Sumar('1,2')).toBe(3);
+});
 
 test("Suma de strings: '4,2' debe ser 6", () => {
-    const suma = new SumaDeStrings()
-    expect(suma.Sumar("4,2")).toBe(6)
-})
+  const suma = new SumaDeStrings();
+  expect(suma.Sumar('4,2')).toBe(6);
+});
+
+test("Suma de 1 string: '1' debe ser 1", () => {
+  const suma = new SumaDeStrings();
+  expect(suma.Sumar('1')).toBe(1);
+});
