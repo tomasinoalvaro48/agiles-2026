19be92064953c94ca2981b5eae17d3149b450a9d
Alvaro Tomasino <alvaroyaugusto@gmail.com>
2026-06-08 21:59:20 -0300

    coverage fix 2

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>'],
   testMatch: ['**/*.test.ts', '**/*.spec.ts', '**/test.ts'],
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['html', 'lcov', 'text-summary'],
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
